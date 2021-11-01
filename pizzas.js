21e58bba172cf91abf493dcc2f5d817f9364923d
Thiago Gaitkoski <68345301+ThiagoGaitkoski@users.noreply.github.com>
2021-11-01 17:06:01 -0300

    Update pizzas.js

--- pizzas.js
+++ pizzas.js
@@ -5,5 +5,5 @@ let pizzaJson = [
     {id: 4, name: 'Quatro Queijos', img: 'images/pizza4.png', price: 21.43, sizes: ['110g', '530g', '860g'], description:'Mozzarella, catupiry, provolone, gorgonzola e parmesão'},
     {id: 5, name: 'Americana', img: 'images/pizza5.png', price: 19.77, sizes: ['110g', '530g', '860g'], description:'Molho de tomate, mussarela e presunto'},
     {id: 6, name: 'Moda da Casa', img: 'images/pizza6.png', price: 18.50, sizes: ['110g', '530g', '860g'], description:'Queijo mozzarella, presunto, calabresa defumada, bacon, catupiry, milho, orégano'},
-    {id: 7, name: 'Chocolate', img: 'images/pizza7.png', price: 24.40, sizes: ['110g', '530g', '860g'], description:'Chocolate ao leite, e confetes MMs'}
+    {id: 7, name: 'Chocolate', img: 'images/pizza7.png', price: 24.40, sizes: ['110g', '530g', '860g'], description:'Chocolate ao leite, e confeitos MMs'}
 ]
